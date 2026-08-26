7481329067cc710f7aadc32d08512be7abe6dc9d
usertypo <usertypo@gmail.com>
2026-08-26 17:06:32 +0530

    fix: re-render orbit avatar when level changes after enrichment

--- js/router.js
+++ js/router.js
@@ -45,7 +45,7 @@
     function ensureRouteScripts(path) {
         var jobs = [];
         if (path === '/room') {
-            jobs.push(loadScriptOnce('js/pages/room-race.js?v=45'));
+            jobs.push(loadScriptOnce('js/pages/room-race.js?v=46'));
         }
         if (path === '/dual') {
             jobs.push(loadScriptOnce('js/pages/dual-race.js?v=35'));
